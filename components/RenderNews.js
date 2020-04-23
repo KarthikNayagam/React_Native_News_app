cfce1f988050d9b05818f0ad4c218e56c847f083
karthik.n1@photoninfotech.net <karthik.n1@photon.in>
2020-04-23 22:47:08 +0530

    Added display for each category

--- components/RenderNews.js
+++ components/RenderNews.js
@@ -1,11 +1,51 @@
-import React from 'react';
-import {View, Text} from 'native-base';
+import React, {useEffect, useState} from 'react';
+import {ScrollView} from 'react-native';
 
-const RenderNews = ({category}) => {
+import {
+  List,
+  ListItem,
+  Thumbnail,
+  Text,
+  Left,
+  Body,
+  Right,
+  Button,
+  Spinner,
+} from 'native-base';
+const RenderNews = ({category, loading, content}) => {
   return (
-    <View>
-      <Text>{category}</Text>
-    </View>
+    <>
+      {loading ? (
+        <Spinner color="blue" />
+      ) : (
+        <ScrollView>
+          {content &&
+            content.map((obj) => {
+              return (
+                <List>
+                  <ListItem thumbnail>
+                    <Left>
+                      <Thumbnail square source={{uri: obj.urlToImage}} />
+                    </Left>
+                    <Body>
+                      {/* <Text>Sankhadeep</Text>
+            <Text note numberOfLines={1}>
+              Its time to build a difference . .
+            </Text> */}
+                      <Text>{obj.title}</Text>
+                    </Body>
+                    <Right>
+                      <Button transparent>
+                        <Text>View</Text>
+                      </Button>
+                    </Right>
+                  </ListItem>
+                </List>
+              );
+            })}
+        </ScrollView>
+      )}
+    </>
   );
 };
 
--- components/TabHeader.js
+++ components/TabHeader.js
@@ -1,4 +1,4 @@
-import React, {Component, useState} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 import {
   Container,
   Header,
@@ -9,37 +9,53 @@ import {
 } from 'native-base';
 import HeaderTitle from './Header';
 import RenderNews from './RenderNews';
+import axios from 'axios';
 const TabHeader = () => {
   const [category, setCategory] = useState('General');
+  const [content, setContent] = useState([]);
+  const [loading, setLoading] = useState(true);
   const handleTabChange = ({ref}) => {
     setCategory(ref.props.heading);
+    setLoading(true);
   };
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://newsapi.org/v2/top-headlines?country=in&category=${category}&apiKey=2d44fa08b51e41a0b4e0c314e0c76c18`,
+      )
+      .then((response) => {
+        setContent(response.data.articles);
+        setLoading(false);
+      });
+  }, [category]);
   return (
     <Container>
       <HeaderTitle />
       <Tabs
         renderTabBar={() => <ScrollableTab />}
-        onChangeTab={(e) => handleTabChange(e)}>
+        onChangeTab={(e) => handleTabChange(e)}
+        tabDelay={0}>
         <Tab heading="General">
-          <RenderNews category={category} />
+          <RenderNews category={category} content={content} loading={loading} />
         </Tab>
         <Tab heading="Health">
-          <RenderNews category={category} />
+          <RenderNews category={category} content={content} loading={loading} />
         </Tab>
         <Tab heading="Technology">
-          <RenderNews category={category} />
+          <RenderNews category={category} content={content} loading={loading} />
         </Tab>
         <Tab heading="Business">
-          <RenderNews category={category} />
+          <RenderNews category={category} content={content} loading={loading} />
         </Tab>
         <Tab heading="Entertainment">
-          <RenderNews category={category} />
+          <RenderNews category={category} content={content} loading={loading} />
         </Tab>
         <Tab heading="Sports">
-          <RenderNews category={category} />
+          <RenderNews category={category} content={content} loading={loading} />
         </Tab>
         <Tab heading="Science">
-          <RenderNews category={category} />
+          <RenderNews category={category} content={content} loading={loading} />
         </Tab>
       </Tabs>
     </Container>
